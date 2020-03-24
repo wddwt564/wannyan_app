ff47a381e9291df4a627eb79e07019a41fcbfbc6
EC2 Default User <ec2-user@ip-172-31-89-219.ec2.internal>
2020-03-24 12:19:29 +0000

    テスト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-config.action_controller.default_url_options = {:host => 'https://guarded-dusk-04985.herokuapp.com'}
+# config.action_controller.default_url_options = {:host => 'https://guarded-dusk-04985.herokuapp.com'}
+
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'https://guarded-dusk-04985.herokuapp.com' }
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
