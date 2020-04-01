8e2e9f057d316bf4c9719c9131150ca8a8cd2642
EC2 Default User <ec2-user@ip-172-31-89-219.ec2.internal>
2020-04-01 16:09:53 +0000

    migration-file_users

---
+++ db/migrate/20200331130604_add_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :user_name, :string
+    add_column :users, :gender, :string
+    add_column :users, :age, :integer
+    add_column :users, :profile, :text
+  end
+end
