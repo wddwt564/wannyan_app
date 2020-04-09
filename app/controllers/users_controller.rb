222adf775bdcbf2ba6e0751a2b9f8dc2347d7106
EC2 Default User <ec2-user@ip-172-31-89-219.ec2.internal>
2020-04-09 05:43:47 +0000

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def update
       current_user.update(update_params)
       
-      redirect_back(fallback_location: root_path)
+      # redirect_back(fallback_location: root_path)
   end
   
     private
