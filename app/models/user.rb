c00a3b8c0da9270d955b700b340418e7055e71c5
EC2 Default User <ec2-user@ip-172-31-89-219.ec2.internal>
2020-04-12 09:07:28 +0000

    コミットメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable,  :validatable, :confirmable
+         
+   def full_profile?
+    image.attached?  && user_name? && age?
+  end
+
 end
