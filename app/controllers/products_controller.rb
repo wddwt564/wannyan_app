4a16f470ac9db8fa333b32af46cc505f133e216c
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-04-22 19:14:44 +0000

    rooms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,10 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!,except: :user_create
   def show
-    @room = room.new
+    @room = Room.new
   end
   
   def index
-    @room = room.new
   end
   
 end
