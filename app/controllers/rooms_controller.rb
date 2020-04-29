f4c70f2c21e041aa6ee7432473508f800c8cedef
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-04-29 22:56:44 +0000

    update

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,20 @@
+class RoomsController < ApplicationController
+  def show
+    @room = Room.new
+    @rooms = current_user.rooms
+  end
+  
+  def new
+    @room = Room.new
+    @rooms = current_user.rooms
+  end
+  
+  def create
+    Room.create(create_params)
+  end
+
+  private
+  def create_params
+    params.require(:room).permit(:text, :image).merge(user_id: current_user.id)
+  end
+end
