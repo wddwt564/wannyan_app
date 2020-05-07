33f166d2e141e301bf380c708f5ad841b80f6b3e
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-05-07 08:02:51 +0000

    create

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,7 +6,7 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
     @rooms = current_user.rooms
-    redirect_to :action => 'create_ramms'
+    redirect_to :action => 'create'
   end
   
   def create
