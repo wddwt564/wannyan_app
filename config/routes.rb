9f17cd699d656a41e69af821ee05c3e56a6a939f
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-05-13 09:47:23 +0000

    コミットメッセージ

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
     get 'products'  =>  'products#show'
     get 'products/user_create'  =>  'products#user_create'
     resources :users, only: [:show, :edit, :update]
-    resources :rooms, only: [:new,:create,:index]
-    get 'rooms/create_rooms' =>  'rooms#create_rooms'
-    
+    # resources :rooms, only: [:new,:create,:index]
+    # get 'rooms/create_rooms' =>  'rooms#create_rooms'
+      resources :rooms do
+    collection do
+      get 'create_rooms'
+    end
+  end
 end
