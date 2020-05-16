050ef5f35ffffef3a048417c3a415d0d026b3f46
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-05-16 08:19:37 +0000

    コミットメッセージ

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,7 @@
 class Room < ApplicationRecord
     belongs_to :user
+  validates :image, length: { minimum: 2 },  allow_nil: true
   has_one_attached :image
-  validates_presence_of :user_id, :text, :image
+  validates_presence_of :user_id, :text
+  validates :user_id, :text, presence: { message: "入力してください" }
 end
