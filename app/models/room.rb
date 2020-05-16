2bc3162fb777b8489e0974204f10b03a6a7eaf00
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-05-16 12:06:40 +0000

    コミットメッセージ

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,15 +5,15 @@ class Room < ApplicationRecord
   validates_presence_of :user_id, :text
   validates :genre, :room_name, :text, presence: { message: "入力してください" }
   
-  validate :image, if: :was_attached?
+#   validate :image, if: :was_attached?
 
-  def image_content_type
-    extension = ['image/png', 'image/jpg', 'image/jpeg']
-    errors.add(:image, "の拡張子が間違っています") unless image.content_type.in?(extension)
-  end
+#   def image_content_type
+#     extension = ['image/png', 'image/jpg', 'image/jpeg']
+#     errors.add(:image, "の拡張子が間違っています") unless image.content_type.in?(extension)
+#   end
 
-  def was_attached?
-    self.image.attached?
-  end
+#   def was_attached?
+#     self.image.attached?
+#   end
   
 end
