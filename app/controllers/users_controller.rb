f613a913df1e74e4046e32423a65d0af00fb92ad
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-05-21 11:29:55 +0000

    コミットメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @myroom = Room.order('id ASC').limit(5)
+    # @myroom = Room.order('id ASC').limit(5)
   end
   
   def edit
