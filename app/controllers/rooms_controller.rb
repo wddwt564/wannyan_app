0b8353296e5352eb3ab2070e1a8d6c575282b1ec
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-05-28 17:19:20 +0000

    コミットメッセージ

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,8 +2,8 @@ class RoomsController < ApplicationController
   
   def index
     @rooms = current_user.rooms.order("id DESC")
-    @posts = Room.find_by(id: params[:id])
-    @user = User.find_by(id: @posts.user_id)
+    # @post = Room.find_by(id: params[:id])
+    # @user = User.find_by(id: @post.user_id)
   end
   
   
