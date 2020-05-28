4dc34455761d1dc2e8f03d418ef39f3e44ba9fc4
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-05-28 19:40:37 +0000

    コミットメッセージ

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,16 +4,6 @@ class Room < ApplicationRecord
   has_one_attached :image
   validates_presence_of :user_id, :text
   validates :genre, :room_name, :text, presence: { message: "入力してください" }
-  
-#   validate :image, if: :was_attached?
-
-#   def image_content_type
-#     extension = ['image/png', 'image/jpg', 'image/jpeg']
-#     errors.add(:image, "の拡張子が間違っています") unless image.content_type.in?(extension)
-#   end
-
-#   def was_attached?
-#     self.image.attached?
-#   end
+  validates :room_name,length: { maximum: 25 }
   
 end
