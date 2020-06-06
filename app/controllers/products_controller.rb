4a109b900e789f4362a23cd69b6b2e1423d18651
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-06-06 07:46:53 +0000

    コミットメッセージ

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!,except: :user_create
   def show
     @room = Room.new
+    @allrooms = Room.all.order('id DESC').limit(5)
   end
   
 end
