c924ada6191bf8636fe8454937bcd25e55070193
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-06-23 10:40:31 +0000

    neko

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!,except: :user_create
+  
   def show
     @room = Room.new
     @allrooms = Room.all.order('id DESC').limit(5)
