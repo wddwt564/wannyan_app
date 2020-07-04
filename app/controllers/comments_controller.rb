4ea54b7cf0a7f7d2a0831f2d833edaba7e8884da
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-07-04 07:09:16 +0000

    neko

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:room_id], user_id: current_user.id)
+    redirect_to "/tweets/#{@comment.room.id}"   #コメントと結びつくツイートの詳細画面に遷移する
+  end
+
+  private
+  def comment_params
+    params.permit(:text, :room_id)
+  end
+end
