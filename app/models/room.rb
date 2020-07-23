f88520bcb15644ecd0319687a3d7ad5451d52e07
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-07-23 10:21:43 +0000

    neko

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,8 +4,8 @@ class Room < ApplicationRecord
   validates :image, length: { minimum: 2 },  allow_nil: true
   has_one_attached :image
   validates_presence_of :user_id, :text
-  validates :genre, :room_name, :text, presence: { message: "入力してください" }
-  validates :room_name,length: { maximum: 25 } ,presence: { message: "入力してください" }
+  validates :genre, :room_name, :text, presence: { message: "を入力してください" }
+  validates :room_name,length: { maximum: 25 } ,presence: { message: "を入力してください" }
   
   def self.search(search)
   if search
