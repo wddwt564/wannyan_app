65c028da19402cf422c0e837dd4c12e3dc585e08
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-07-23 08:29:27 +0000

    neko

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
-
+config.i18n.default_locale = :ja
 module Wannyan
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -17,5 +17,6 @@ module Wannyan
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = :ja
   end
 end
