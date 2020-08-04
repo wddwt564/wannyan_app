22fa9e9222875bf1e29b3ce32bc2d188c849ed0f
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-08-04 02:37:26 +0000

    neko

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,6 @@ class CommentsController < ApplicationController
   
   private
   def comment_params
-    params.require(:comment).permit(:text, :room_id)
+    params.permit(:text, :room_id)
   end
 end
