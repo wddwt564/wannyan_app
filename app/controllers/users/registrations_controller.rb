40de563678d6d5d571aeebf431350ae5712cf4c0
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-08-06 20:30:35 +0000

    neko

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -26,12 +26,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # DELETE /resource
   def destroy
-        # userモデルのleaveメソッド
-    resource.leave
+    resource.soft_delete
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-    set_flash_message(:notice, :destroyed)
+    set_flash_message :notice, :destroyed
     yield resource if block_given?
     respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
+      # Devise registrations controllerを継承し、destroyをオーバーライドする。
   end
 
   # GET /resource/cancel
