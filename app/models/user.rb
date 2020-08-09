b1368add4145fcc297ceab80710b0f0fd00d2122
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-08-09 04:30:30 +0000

    neko

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,28 +16,10 @@ class User < ApplicationRecord
   validates_format_of :email, with: Devise.email_regexp, if: :will_save_change_to_email?
   validates :password, presence: true, confirmation: true, length: { in: Devise.password_length }, on: :create
   validates :password, confirmation: true, length: { in: Devise.password_length }, allow_blank: true, on: :update
-  
-    # 物理削除の代わりにユーザーの`deleted_at`をタイムスタンプで更新
-  def soft_delete  
-    update_attribute(:deleted_at, Time.current)  
-  end
 
-  # ユーザーのアカウントが有効であることを確認 
-  def active_for_authentication?  
-    super && !deleted_at  
-  end  
-
-  # 削除したユーザーにカスタムメッセージを追加します  
-  def inactive_message   
-    !deleted_at ? super : :deleted_account  
-  end 
-  
+  # @see https://github.com/heartcombo/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     self.without_soft_deleted.where(conditions.to_h).first
   end
-  
-  #   def active_for_authentication?
-  #   super && (self.is_deleted == false)
-  # end
 end
