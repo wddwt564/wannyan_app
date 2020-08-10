8017feeeca07f5ecc3e82a71481eb56a210b2743
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-08-10 21:10:26 +0000

    neko

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,15 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
   
+  def destroy
+    # @user = User.find(params[:id])
+    # @user.destroy
+    # reset_session
+    # flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
+    # redirect_to root_path
+  end
+  
+  
     private
   def update_params
     params.require(:user).permit(:user_name, :gender, :age, :profile,:image)
