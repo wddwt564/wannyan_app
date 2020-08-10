fe7526eb58d54108c2d67fbee7ed9e05d2e8ad8b
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-08-10 19:59:30 +0000

    neko

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,15 +24,5 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-    def reject_user
-    @user = User.find_by(email: params[:user][:email].downcase)
-    if @user
-      if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
-        flash[:error] = "退会済みです。"
-        redirect_to new_user_session_path
-      end
-    else
-      flash[:error] = "必須項目を入力してください。"
-    end
-  end
+  
 end
