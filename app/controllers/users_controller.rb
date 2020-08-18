f7619a4e146b90f1e25a22ddd9f3c2583010fd1f
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-08-18 19:22:09 +0000

    neko

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,10 @@ class UsersController < ApplicationController
     before_action :redirect, only: :new
   def show
     if User.where.not(deleted_at: nil).exists?
-       redirect_to :roots
-    end
+    else
     @user = User.find(params[:id])
     @myroom = @user.rooms.includes(:user).order('id DESC').limit(5)
+    end
   end
   
   def edit
