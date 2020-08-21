ecbd5dbceee2d5177a8ad1f07022765d52bc4526
EC2 Default User <ec2-user@ip-172-31-81-28.ec2.internal>
2020-08-21 10:37:35 +0000

    neko

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
     before_action :redirect, only: :new
   def show
-    @delete = User.find_by(deleted_at: params[:deleted_at])
+    @delete = User.all
     @user = User.find(params[:id])
     @myroom = @user.rooms.includes(:user).order('id DESC').limit(5)
     @mycomment = @user.comments.includes(:user).order('id DESC').limit(5)
